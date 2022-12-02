83998146e35f6a34d07f11e7d72173facac64483
Koushik Das <koushik78bd@gmail.com>
2022-12-03 00:13:50 +0600

    bs

---
+++ BS-Celling.js
@@ -0,0 +1,43 @@
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+function FlooringAndCling(arr, target) {
+  let start = 0
+  let end = arr.length - 1
+  let middel
+  let chackAscDsc
+  let obj = {}
+  if (arr[start] < arr[end]) {
+    chackAscDsc = true
+  } else {
+    chackAscDsc = false
+  }
+
+  while (start <= end) {
+    middel = Math.floor((start + end) / 2)
+    if (arr[middel] === target) {
+      obj['flooring'] = arr[middel - 1]
+      obj['cling'] = arr[middel]
+      return obj
+    } else if (chackAscDsc) {
+      if (arr[middel] < target) {
+        start = middel + 1
+      } else {
+        end = middel - 1
+      }
+    } else {
+      if (arr[middel] > target) {
+        start = middel + 1
+      } else {
+        end = middel - 1
+      }
+    }
+  }
+
+  obj['flooring'] = arr[middel - 1]
+  obj['cing'] = arr[middel]
+  // return -1
+  return obj
+}
+
+const output = FlooringAndCling(ss, 9) // {floring: 8, cling: 9}
+const output1 = FlooringAndCling(ss, 2) //{flooring: 1, cling: 2}
+console.log(output)
