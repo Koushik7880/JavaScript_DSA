a300d3c58c02170df0db17ce577ba87478bc7a65
unknown <maheen@DEV-MLT010-DP.maheenlabel.local>
2022-12-06 18:07:44 +0600

    s

--- BinarySearch/FindTargetIndicesAfterSortingArray.js
+++ BinarySearch/FindTargetIndicesAfterSortingArray.js
@@ -1,34 +1,53 @@
-var targetIndices = function (nums, target) {
-  let start = 0
-  let end = nums.length - 1
-  let i
-  let asc
-  if (nums[start] < nums[end]) {
-    asc = true
-  } else {
-    asc = false
-  }
+function targetIndices(nums, target) {
+  nums.sort(function(a, b) {
+  return a - b;
+});
 
+  let start = 0;
+  let end = nums.length - 1;
+  let mid;
   while (start <= end) {
-    i = Math.floor((start + end) / 2)
-    if (nums[i] === target) {
-      return i
-    }
-    if (asc) {
-      if (nums[i] < target) {
-        start = i + 1
-      } else {
-        end = i - 1
-      }
-    } else {
-      if (nums[i] > target) {
-        start = i + 1
-      } else {
-        end = i - 1
-      }
-    }
+          let count = [];
+          //Calculate Mid Point:
+          mid = Math.floor((start + end) / 2);
+
+          //When Target Value Match:
+          if (target === nums[mid]) {
+                  count.push(mid);
+                  //Check Count for left side:
+                  for (let i = mid - 1; i >= 0; i--) {
+                          if (target === nums[i]) {
+                                  count.push(i);
+                          }
+                  }
+                  //Check Count for right side:
+                  for (let i = mid + 1; i < nums.length; i++) {
+                          if (target === nums[i]) {
+                                  count.push(i);
+                          }
+                  }
+                  return count.sort();
+          }
+          //Update Star and End Point for control loop:
+          else if (target > nums[mid]) {
+                  start = mid + 1;
+          }
+          else {
+                  end = mid - 1;
+          }
   }
-  return [i]
+  return [];
 }
 
-console.log(targetIndices([1, 2, 5, 2, 3], 2))
+console.log(targetIndices([100,100,100,100,100,100,100,,100,100,100],100));
+// console.log(targetIndices([1,2,5,2,3],2));
+// console.log(targetIndices([1,2,5,2,3],3));
+// console.log(targetIndices([1,2,5,2,3],5));
+
+// var numArray = [100,100,100,100,100,100,100,
+//   100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100];
+// numArray.sort(function(a, b) {
+//   return a - b;
+// });
+
+// console.log(numArray); 
--- test2.js
+++ test2.js
@@ -62,3 +62,74 @@ console.log(binarySearch([10,20,30,40,50,60,70,80,90,100], 50))
  }
 
  console.log(OrderAgnosticBinarySearch([10,20,30,40,50,60,70,80,90,], 70));
+
+ ///////////////////////////////////  Order Agnosted BS ////////////////////////////////
+
+
+
+var targetIndices = function(nums, target) {
+    nums.sort();
+    console.log(nums.sort());
+    let start = 0;
+    let end = nums.length - 1;
+    let mid = Math.floor((start+end)/2);
+    while (start <= end){
+        if(nums[mid] === target){
+            return mid
+        } else if (nums[mid] < target) {
+            start = mid + 1
+            mid = Math.floor((start+end)/2);
+        } else {
+            end = mid - 1
+            mid = Math.floor((start + end)/2);
+        }
+    }
+};
+console.log(targetIndices([1,2,5,2,3], 2));
+
+
+
+let arr = [2, 5, 8, 8, 8, 8, 8, 9, 12, 12];
+let target = 8;
+
+function numberOfOccurrence(nums, target) {
+        // if (!nums.length) {
+        //         return [-1, -1];
+        // }
+        let start = 0;
+        let end = nums.length - 1;
+        let mid;
+        while (start <= end) {
+                let count = [];
+                //Calculate Mid Point:
+                mid = Math.floor((start + end) / 2);
+
+                //When Target Value Match:
+                if (target === nums[mid]) {
+                        count.push(mid);
+                        //Check Count for left side:
+                        for (let i = mid - 1; i >= 0; i--) {
+                                if (target === nums[i]) {
+                                        count.push(i);
+                                }
+                        }
+                        //Check Count for right side:
+                        for (let i = mid + 1; i < nums.length; i++) {
+                                if (target === nums[i]) {
+                                        count.push(i);
+                                }
+                        }
+                        return count.sort();
+                }
+                //Update Star and End Point for control loop:
+                else if (target > nums[mid]) {
+                        start = mid + 1;
+                }
+                else {
+                        end = mid - 1;
+                }
+        }
+        return [];
+}
+
+console.log(numberOfOccurrence([2, 5, 8, 8, 8, 8, 8, 9, 12, 12], 8));
