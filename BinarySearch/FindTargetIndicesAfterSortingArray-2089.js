d3355c5973501384d1573e7e258e0a5302347c08
unknown <maheen@DEV-MLT010-DP.maheenlabel.local>
2022-12-10 17:22:17 +0600

    searchInsert

--- BinarySearch/FindTargetIndicesAfterSortingArray-2089.js
+++ BinarySearch/FindTargetIndicesAfterSortingArray-2089.js
@@ -1,43 +1,44 @@
-function targetIndicesss(nums, target) {
-    nums.sort(function(a, b) {
-    return a - b;
-  });
+// function targetIndices(nums, target) {
+//     nums.sort(function(a, b) {
+//     return a - b;
+//   });
   
-    let start = 0;
-    let end = nums.length - 1;
-    let mid;
-    while (start <= end) {
-            let count = [];
-            //Calculate Mid Point:
-            mid = Math.floor((start + end) / 2);
+//     let start = 0;
+//     let end = nums.length - 1;
+//     let mid;
+//     while (start <= end) {
+//             let count = [];
+//             //Calculate Mid Point:
+//             mid = Math.floor((start + end) / 2);
   
-            //When Target Value Match:
-            if (target === nums[mid]) {
-                    count.push(mid);
-                    //Check Count for left side:
-                    for (let i = mid - 1; i >= 0; i--) {
-                            if (target === nums[i]) {
-                                    count.push(i);
-                            }
-                    }
-                    //Check Count for right side:
-                    for (let i = mid + 1; i < nums.length; i++) {
-                            if (target === nums[i]) {
-                                    count.push(i);
-                            }
-                    }
-                    return count.sort();
-            }
-            //Update Star and End Point for control loop:
-            else if (target > nums[mid]) {
-                    start = mid + 1;
-            }
-            else {
-                    end = mid - 1;
-            }
-    }
-    return [];
-  }
+//             //When Target Value Match:
+//             if (target === nums[mid]) {
+//                     count.push(mid);
+//                     //Check Count for left side:
+//                     for (let i = mid - 1; i >= 0; i--) {
+//                             if (target === nums[i]) {
+//                                     count.push(i);
+//                             }
+//                     }
+//                     //Check Count for right side:
+//                     for (let i = mid + 1; i < nums.length; i++) {
+//                             if (target === nums[i]) {
+//                                     count.push(i);
+//                             }
+//                     }
+//                     return count.sort();
+//             }
+//             //Update Star and End Point for control loop:
+//             else if (target > nums[mid]) {
+//                     start = mid + 1;
+//             }
+//             else {
+//                     end = mid - 1;
+//             }
+//     }
+//     return [];
+//   }
+  
 
   
   //////////////////////////////////////////////////////////////////////////////////////////////// 
