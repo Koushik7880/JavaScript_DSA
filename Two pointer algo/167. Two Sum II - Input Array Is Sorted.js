b320b11c05a7114fc3af72997f45bf4ecf8786c5
Koushik <koushik@it-continental.com>
2023-01-04 17:32:56 +0600

    s

--- Two pointer algo/167. Two Sum II - Input Array Is Sorted.js
+++ Two pointer algo/167. Two Sum II - Input Array Is Sorted.js
@@ -17,10 +17,54 @@ var twoSumm = function(numbers, target) {
 console.log(twoSumm([2,7,11,15],9));
 
 
-var twoSum = function (numbers, target) {
+var twoSummm = function (numbers, target) {
     let left = 0;
     let right = numbers.length-1;
     while(left<right){
-        // const 
+        const valu = numbers[left] + numbers[right];
+        // Return 1 base index if the current sum is equal to the target
+        if (valu === target){
+            return [left+1, right+1];
+            
+        }
+        if(valu > target){
+            return [right--]  
+        }
+        
+        // valu > target ? right-- : left++
+       
     }
 }
+
+console.log(twoSummm([2,7,11,15],9));
+
+
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+    let s=0;
+    let e= numbers.length-1;
+    let sum=0;
+ 
+    while(s<e){
+     sum=numbers[s]+numbers[e];
+     if(sum==target){
+         return [s+1,e+1];
+     }
+     else{
+         if(sum > target){
+             e-- 
+         }else{
+         s++;
+         }
+     }
+     
+    }
+    return [];
+ };
+ 
+ let numbers = [2,7,11,15]; target = 9;
+ console.log(twoSum(numbers, target))
