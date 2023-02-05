732e0545d0ec66d45a32d5d76ba87e9585df74a6
Koushik <koushik@it-continental.com>
2023-02-05 17:49:30 +0600

    link list

---
+++ LinkedList/Linked_CRUD.js
@@ -0,0 +1,65 @@
+
+////////////////////////////////////////////// Append /////////////////////////////////////////////
+
+// class Node {
+//     constructor(value, next = null){
+//         this.value = value,
+//         this.next = next
+//     }
+//     }
+
+// class LinkedList{
+//     constructor(value){
+//         let node = new Node(value)
+//         this.head = node,
+//         this.tail = node 
+//     }
+//     append(value){
+//         let node = new Node(value)
+//           this.tail.next = node
+//           this.tail = node
+//     }
+// }
+
+// let list = new LinkedList(100)
+// list.append(200)
+// console.log(list);
+
+/////////////////////////////////////////// Prepand //////////////////////////////////////
+
+class Node {
+    constructor(value, next = null){
+        this.value = value,
+        this.next = next
+    }
+    }
+
+class LinkedList{
+    constructor(value){
+        let node = new Node(value)
+        this.head = node,
+        this.tail = node 
+    }
+
+    append(value){
+                let node = new Node(value)
+                  this.tail.next = node
+                  this.tail = node
+            }
+    
+    prepend(value){
+        // 1. create new node
+        let node = new Node(value)
+        // 2. new node next will be head
+        node.next = this.head
+        // 3. update head 
+        this.head = node
+    }
+}
+
+let list = new LinkedList(10)
+list.append(20)
+list.append(30)
+list.prepend(5)
+
+console.log(list);
--- LinkedList/day1.js
+++ LinkedList/day1.js
@@ -1,5 +1,68 @@
-class LinkedList {
+// class Node {
+//     constructor(value, next = null){
+//         this.value = value,
+//         this.next = next
+//     }
+//     }
+//     let node = new Node(100)
+//     console.log(node);
 
-}
+//     class LinkedList{
+//         constructor(value){
+//             let node = new Node(value)
+//             console.log(node);
+//         }
+//     }
+    
+    // let list = new LinkedList(100)
+    ///////////////////////////////////////////////////////////////////////////////////////
 
-new LinkedList(100)
+    // class Node {
+    //     constructor(value, next = null){
+    //         this.value = value,
+    //         this.next = next
+    //     }
+    //     }
+        // class LinkedList{
+        //     constructor(value){
+        //         let node = new Node(value)
+        //         this.head = node
+        //         this.tail = node
+        //     }
+        // }
+        
+        // let list = new LinkedList(100)
+        // console.log(list)
+
+        ///////////////////////////////////////////////////////////////////////////////////////
+
+        class Node {
+            constructor(value, next = null){
+                this.value = value,
+                this.next = next
+            }
+            }
+
+        class LinkedList{
+            constructor(value){
+                let node = new Node(value)
+                this.head = node,
+                this.tail = node 
+            }
+        
+            append(value){
+                let node = new Node(value)
+                  this.tail.next = node
+                  this.tail = node
+            }
+        }
+        let list = new LinkedList(100)
+        list.append(200)
+// list.append(300)
+// list.append(400)
+// list.append(500)
+// list.append(600)
+        console.log(list);
+
+
+        ///////////////////////////////////////////////////////////////////////////////////////
